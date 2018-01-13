1bb6bc9fd3d030e68963cb429455832faf45b733
Swapna <swapna.ve@gmail.com>
2018-01-13 17:28:38 +0530

    Structured Project with proper Model, Controller and Views
    Structured Project with proper Model, Controller and Views

---
+++ PersonSearch/PersonSearch/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonSearch.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public ActionResult AdvancedSearch()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PersonSearch/PersonSearch/Models/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonSearch.Models
+{
+    public class People
+    {
+        public Int64 ID { get; set; }
+        public string NAME { get; set; }
+        public string GENDER { get; set; }
+        public string BIRTHPLACE { get; set; }
+        public Int64 FATHER_ID { get; set; }
+        public Int64 MOTHER_ID { get; set; }
+        public Int32 LEVEL { get; set; }
+    }
+
+}
