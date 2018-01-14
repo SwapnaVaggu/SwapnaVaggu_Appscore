e32e912bff6f89f18e96f64f4a94cded1fa3ca9a
Swapna <swapna.ve@gmail.com>
2018-01-14 05:48:34 +0530

    Included Logic for paging

--- PersonSearch/PersonSearch/Controllers/SearchController.cs
+++ PersonSearch/PersonSearch/Controllers/SearchController.cs
@@ -15,7 +15,7 @@ namespace PersonSearch.Controllers
     {
         public ActionResult SimpleSearch()
         {
-            string file = Server.MapPath("~/App_Data/data_small.json");
+            string file = Server.MapPath("~/App_Data/data_large.json");
             //deserialize JSON from file  
             string JSONresult = System.IO.File.ReadAllText(file);
 
@@ -41,11 +41,71 @@ namespace PersonSearch.Controllers
             string JSONFinalresult = JsonConvert.SerializeObject(join);
             List<People> myDeserializedObjList = (List<People>)Newtonsoft.Json.JsonConvert.DeserializeObject(JSONFinalresult, typeof(List<People>));
 
+            SortingPagingInfo info = new SortingPagingInfo();
+            info.SortField = "ID";
+            info.SortDirection = "ascending";
+            info.PageSize = 10;
+            int rem = myDeserializedObjList.Count() % info.PageSize;
+            if (rem == 0)
+            {
+                info.PageCount = Convert.ToInt32(Math.Ceiling((double)(myDeserializedObjList.Count() / info.PageSize)));
+            }
+            else
+            {
+                info.PageCount = Convert.ToInt32(Math.Ceiling((double)(myDeserializedObjList.Count() / info.PageSize))) + 1;
+            }
+            info.CurrentPageIndex = 0;
 
-            return View(myDeserializedObjList);
+            var query = myDeserializedObjList.AsQueryable().Take(info.PageSize);
+
+            ViewBag.SortingPagingInfo = info;
+            List<People> model = query.ToList();
+
+            return View(model);
         }
 
-      
+        [HttpPost]
+        public ActionResult SimpleSearch(SortingPagingInfo info)
+        {
+            string file = Server.MapPath("~/App_Data/data_large.json");
+            //deserialize JSON from file  
+            string JSONresult = System.IO.File.ReadAllText(file);
+            IQueryable<People> query = null;
+            List<People> model = new List<People>();
+
+            JObject JObject = JObject.Parse(JSONresult);
+            JArray people = (JArray)JObject["people"];
+            JArray places = (JArray)JObject["places"];
+
+
+            var join = people.Join(places, s => s["place_id"].Value<string>(),
+                                  d => d["id"].Value<string>(),
+                                  (s, d) => new {
+                                      ID = s["id"].Value<string>(),
+                                      NAME = s["name"].Value<string>(),
+                                      GENDER = s["gender"].Value<string>() == "M" ? "Male" : "Female",
+                                      BIRTHPLACE = d["name"].Value<string>()
+                                  });
+
+         
+                string JSONFinalresult = JsonConvert.SerializeObject(join);
+                List<People> myDeserializedObjList = (List<People>)Newtonsoft.Json.JsonConvert.DeserializeObject(JSONFinalresult, typeof(List<People>));
+
+
+                query = myDeserializedObjList.AsQueryable();
+                query = query.Skip(info.CurrentPageIndex * info.PageSize).Take(info.PageSize);
+
+                ViewBag.SortingPagingInfo = info;
+                model = query.ToList();
+
+            
+
+
+
+            return View(model);
+            //}
+        }
+
         public ActionResult AdvancedSearch()
         {
             return View();
---
+++ PersonSearch/PersonSearch/Models/SortingPagingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonSearch.Models
+{
+    public class SortingPagingInfo
+    {
+        public string SortField { get; set; }
+        public string SortDirection { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int CurrentPageIndex { get; set; }
+
+        public string name { get; set; }
+        public string gender { get; set; }
+    }
+}
